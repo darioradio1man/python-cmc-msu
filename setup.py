cb66390e2be235b5429b10ad81bc026c4d785fc3
Ildar Kharrasov <19darius94@gmail.com>
2019-05-31 00:03:00 +0300

    добавление wheel файла

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 from pathlib import Path
 from os import path
 
@@ -9,12 +9,13 @@ with open(datapath / "README.md") as file:
     long_decription = file.read()
 
 setup(
-    name='Graph calculator visualiser',
+    name='GraphCalculator',
     version='0.0.1',
     author='Ildar Kharrasov, Stanislav Kitaev, Anastasia Mazurenko',
     author_email='kharrasov.radiokha94@yandex.ru',
-    description='',
+    description='Python3 project',
     long_decription=long_decription,
+    packages=find_packages(exclude=['Tests']),
     url='https://github.com/darioradio1man/python-cmc-msu',
     classifiers=[
         'Programming Language :: Python :: 3.5',
@@ -25,7 +26,9 @@ setup(
         'matplotlib==3.1.0',
         'numpy==1.16.4',
         'networkx==2.3',
-        'tkinter==8.6',
         'flake8-per-file-ignores==0.8.1',
     ],
-)
+    locale_src='./GraphCalculator/ru/LC_MESSAGES',
+    package_data={'': '*'},
+    include_package_data=True,
+),
